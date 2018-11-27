d21b5c0c9b7541af2f864ded17f32dc8cfa64e09
nilke1995 <brekhin@bk.ru>
2018-11-28 02:31:32 +0300

    gateway was added

---
+++ gateway-api/src/main/java/GatewayApiApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(GatewayApiApplication.class, args);
+        }
+}
